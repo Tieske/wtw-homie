04e9109a4106afdd24bfb81f4f724b07f9602d03
Thijs Schreijer <thijs@thijsschreijer.nl>
2023-11-01 00:18:51 +0100

    added setpoint and hw-setpoint with config storage

--- wtw-homie.ino
+++ wtw-homie.ino
@@ -28,12 +28,26 @@ ppm 1000 -> 5.0 volt uitgang spanning
 
 De potmeter (blauwe blokje met schroefje aan de bovenzijde) op de pwm->0-10v print
 kan gebruikt worden voor het calibreren.
+
+Setpoints:
+----------
+hw-setpoint: het setpoint waarop de WtW machine is ingesteld (eg. 1000)
+setpoint: het gewenste setpoint vanuit de gebruiker (eg. 800)
+
+Het verschil tussen de setpoints wordt verekend met de gemeten CO2 waarde.
+Stel dat we 900 meten. Dan is de machine tevreden met 1000 als setpoint. Maar de
+gebruiker niet (die wil 800).
+Dus qua ppm nemen we het verschil; 1000 - 800 = 200ppm. En dat tellen we bij de gemeten
+waarde op: 900 + 200 = 1100ppm. En dat geven we aan de WtW. Omdat het nu 100 hoger is
+dan het setpoint van de machine, gaat de ventilatie nu harder lopen.
+
 */
 
 // ----------------------------------------------------------------------------------
 //   Initialization and globals
 // ----------------------------------------------------------------------------------
 
+#include "SPIFFS.h"
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include "wtw-homie.h"   // Check this file for setup and secrets !!!!!
@@ -42,6 +56,10 @@ kan gebruikt worden voor het calibreren.
 WiFiClient espClient;
 PubSubClient client(espClient);
 String deviceId = TIESKE_DEVICE_ID;
+
+// global variables for the 2 setpoints
+uint32_t hwSetpoint = 1000; 
+uint32_t setpoint = 1000;
 
 
 // ----------------------------------------------------------------------------------
@@ -64,6 +82,52 @@ void logMessage(const char *format, ...) {
   va_end(args);
 
   Serial.println(logBuffer);
+}
+
+
+// ----------------------------------------------------------------------------------
+//   Read and write configuration data
+// ----------------------------------------------------------------------------------
+
+
+const char* filePath = "/config.txt"; // File path to store the values
+
+void storeSetpoints() {
+  File file = SPIFFS.open(filePath, "w");
+  if (!file) {
+    logMessage("Failed to open config file for writing");
+    return;
+  }
+
+  // Write the values to the file
+  file.write((uint8_t*)&hwSetpoint, sizeof(uint32_t));
+  file.write((uint8_t*)&setpoint, sizeof(uint32_t));
+  file.close();
+  logMessage("Written to config: setpoint %d, hw-setpoint: %d", setpoint, hwSetpoint);
+  return;
+}
+
+void retrieveSetpoints() {
+  File file = SPIFFS.open(filePath, "r");
+  if (!file) {
+    logMessage("Failed to open config file for writing");
+    return;
+  }
+
+  // Read the values from the file
+  file.readBytes((char*)&hwSetpoint, sizeof(uint32_t));
+  file.readBytes((char*)&setpoint, sizeof(uint32_t));
+  file.close();
+  logMessage("Read from config: setpoint %d, hw-setpoint: %d", setpoint, hwSetpoint);
+  return;
+}
+
+void storageSetup() {
+  if (SPIFFS.begin()) {
+    logMessage("SPIFFS mounted successfully");
+  } else {
+    logMessage("SPIFFS mount failed");
+  }
 }
 
 
@@ -116,9 +180,11 @@ void pwmSetup() {
   logMessage("PWM max ppm %d, duty per ppm: %f", MAX_PPM, dutyPerPpm);
 }
 
-// convert co2 ppm measurement into duty cycle
+// convert co2 ppm measurement into duty cycle, taking into account the setpoints
 uint32_t ppm2duty(uint32_t ppm)
 {
+  uint32_t delta = hwSetpoint - setpoint;
+  ppm = ppm + delta;
   uint32_t duty = floor(dutyPerPpm * ppm);
   if (duty > maxrange) {
     duty = maxrange;
@@ -142,6 +208,10 @@ void writeppm(uint32_t ppm)
 
 String co2PropertyTopic = "homie/" + deviceId + "/inputs/co2";
 String co2PropertySetTopic = "homie/" + deviceId + "/inputs/co2/set";
+String setpointPropertyTopic = "homie/" + deviceId + "/inputs/setpoint";
+String setpointPropertySetTopic = "homie/" + deviceId + "/inputs/setpoint/set";
+String hwSetpointPropertyTopic = "homie/" + deviceId + "/inputs/hw-setpoint";
+String hwSetpointPropertySetTopic = "homie/" + deviceId + "/inputs/hw-setpoint/set";
 String lastValue = "800";
 
 // Check if a received payload is a float
@@ -161,11 +231,33 @@ void callback(char* topic, byte* payload, unsigned int length) {
     String value = String((char*)payload);
     //logMessage("received MQTT data: %s", value);
   
-    if (String(topic) == co2PropertySetTopic) {
+    if (String(topic) == co2PropertySetTopic ||
+        String(topic) == setpointPropertySetTopic ||
+        String(topic) == hwSetpointPropertySetTopic) {
       if (isFloat(value) && value.toFloat() > 0) {
-        lastValue = value;
-        writeppm(floor(lastValue.toFloat()));
-        client.publish(co2PropertyTopic.c_str(), lastValue.c_str(), true);
+
+        // valid topic as well as valid value
+        if (String(topic) == co2PropertySetTopic) {
+          // new CO2 measurement
+          lastValue = value;
+          writeppm(floor(lastValue.toFloat()));
+          client.publish(co2PropertyTopic.c_str(), lastValue.c_str(), true);
+          
+        } else if (String(topic) == setpointPropertySetTopic) {
+          // new co2 setpoint
+          setpoint = floor(value.toFloat());
+          writeppm(floor(lastValue.toFloat()));
+          client.publish(setpointPropertyTopic.c_str(), String(setpoint).c_str(), true);
+          storeSetpoints();
+          
+        } else {  // hwSetpointPropertySetTopic
+          // new hw setpoint
+          hwSetpoint = floor(value.toFloat());
+          writeppm(floor(lastValue.toFloat()));
+          client.publish(hwSetpointPropertyTopic.c_str(), String(hwSetpoint).c_str(), true);
+          storeSetpoints();
+          
+        }
       } else {
         logMessage("'%s' received bad value: '%s'", co2PropertySetTopic, value);
       }
@@ -188,8 +280,9 @@ void publishHomieDeviceDescription() {
   writeTopic("homie/" + deviceId + "/$name",                 "WtW CO2 aansturing", true);
   writeTopic("homie/" + deviceId + "/$nodes",                "inputs", true);
   writeTopic("homie/" + deviceId + "/inputs/$name",          "Input waarden voor CO2 aansturing", true);
-  writeTopic("homie/" + deviceId + "/inputs/$properties",    "co2", true);
+  writeTopic("homie/" + deviceId + "/inputs/$properties",    "co2,setpoint,hw-setpoint", true);
   writeTopic("homie/" + deviceId + "/inputs/$type",          "input-sensor", true);
+  
   writeTopic("homie/" + deviceId + "/inputs/co2",            lastValue, true);
   writeTopic("homie/" + deviceId + "/inputs/co2/$datatype",  "integer", true);
   writeTopic("homie/" + deviceId + "/inputs/co2/$name",      "CO2 meting input", true);
@@ -197,8 +290,24 @@ void publishHomieDeviceDescription() {
   writeTopic("homie/" + deviceId + "/inputs/co2/$settable",  "true", true);
   writeTopic("homie/" + deviceId + "/inputs/co2/$unit",      "ppm", true);
 
+  writeTopic("homie/" + deviceId + "/inputs/setpoint",            String(setpoint), true);
+  writeTopic("homie/" + deviceId + "/inputs/setpoint/$datatype",  "integer", true);
+  writeTopic("homie/" + deviceId + "/inputs/setpoint/$name",      "setpoint gewenst door gebuiker", true);
+  writeTopic("homie/" + deviceId + "/inputs/setpoint/$retained",  "true", true);
+  writeTopic("homie/" + deviceId + "/inputs/setpoint/$settable",  "true", true);
+  writeTopic("homie/" + deviceId + "/inputs/setpoint/$unit",      "ppm", true);
+
+  writeTopic("homie/" + deviceId + "/inputs/hw-setpoint",            String(hwSetpoint), true);
+  writeTopic("homie/" + deviceId + "/inputs/hw-setpoint/$datatype",  "integer", true);
+  writeTopic("homie/" + deviceId + "/inputs/hw-setpoint/$name",      "setpoint zoals op de WtW machine ingesteld", true);
+  writeTopic("homie/" + deviceId + "/inputs/hw-setpoint/$retained",  "true", true);
+  writeTopic("homie/" + deviceId + "/inputs/hw-setpoint/$settable",  "true", true);
+  writeTopic("homie/" + deviceId + "/inputs/hw-setpoint/$unit",      "ppm", true);
+
   // subscribe to setter topic
   client.subscribe(co2PropertySetTopic.c_str(), 1);  // QoS level 1; at least once
+  client.subscribe(setpointPropertySetTopic.c_str(), 1);  // QoS level 1; at least once
+  client.subscribe(hwSetpointPropertySetTopic.c_str(), 1);  // QoS level 1; at least once
 
   // Mark device as ready
   writeTopic("homie/" + deviceId + "/$state", "ready", true);
@@ -227,10 +336,10 @@ void reconnectMQTT() {
           TIESKE_MQTT_USER, 
           TIESKE_MQTT_PASSWORD, 
           ("homie/" + deviceId + "/$state").c_str(),  // LWT: topic
-          1,                                        // LWT: QoS
-          true,                                     // LWT: retain
-          "lost",                                   // LWT: value
-          false))                                   // no clean session
+          1,                                          // LWT: QoS
+          true,                                       // LWT: retain
+          "lost",                                     // LWT: value
+          false))                                     // no clean session
       {  
       Serial.print("Failed to connect to MQTT broker, rc=");
       Serial.println(client.state());
@@ -250,10 +359,13 @@ void reconnectMQTT() {
 
 void setup() {
   setupLogging();
+  //SPIFFS.format();  // uncomment to format the config data, and run once. Make sure to comment out again afterwards!!!!
+  storageSetup();
+  retrieveSetpoints();
   pwmSetup();
   writeppm(floor(lastValue.toFloat()));  // set a base value
   wifiSetup();   // set up and connect
-  setupMQTT();   // only set up, connect is automatic in the lmain loop
+  setupMQTT();   // only set up, connect is automatic in the main loop
 }
 
 void loop() {
@@ -262,4 +374,5 @@ void loop() {
   }
 
   client.loop();
+  delay(50);
 }
